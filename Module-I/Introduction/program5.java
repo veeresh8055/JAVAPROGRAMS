60c205b69ef422b72526f2122dc0d3d37227a889
Veeresh Chared <125709784+veeresh8055@users.noreply.github.com>
2026-01-13 20:44:46 +0530

    Add files via upload

---
+++ Module-I/Introduction/program5.java
@@ -0,0 +1,25 @@
+public class program5 {
+  public static void main(String[] args) {
+    System.out.println(10 + 10 );
+    System.out.println("10"+ 10 );
+    System.out.println(10 + "10" );
+    System.out.println("A" + 10 );
+    System.out.println('A' + 10 );
+    System.out.println('B' + 10 );
+    System.out.println('a' + 10 );
+    System.out.println('b' + 10 );
+    System.out.println('+' + 10 );
+    
+    
+  }   
+}
+
+// output
+//   20
+// 1010
+// 1010
+// A10
+// 75
+// 76
+// 107
+// 108
