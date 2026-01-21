82a5265749783e7e6af7425fea44d0ab260b1589
veeresh8055 <visesync@gmail.com>
2026-01-21 18:37:10 +0530

    Operators in Java

---
+++ Module-I/Operators/program24.java
@@ -0,0 +1,13 @@
+public class program24 {
+
+    public static void main(String[] args) {
+        char ch = 'A';
+        System.out.println(++ch);//B
+        System.out.println(1+ch);//67
+
+        // System.out.println(++7); // Compile Time Error
+
+        // System.out.println(++true);// compile time error
+        
+    }
+}
