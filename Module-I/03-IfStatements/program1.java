d123469386138bd3f93b7a995f3aa33cbf93b111
veeresh8055 <visesync@gmail.com>
2026-01-27 10:50:50 +0530

    If Statements

---
+++ Module-I/03-IfStatements/program1.java
@@ -0,0 +1,14 @@
+public class program1 {
+    public static void main(String[] args) {
+        System.out.println("Program starts ....!");
+
+          if(true){
+            System.out.println("Java Executed.");
+          }
+          if(false){
+            System.out.println("Program nat execeuted ");
+          }
+
+        System.out.println("Program ends ....!");
+    }
+}
